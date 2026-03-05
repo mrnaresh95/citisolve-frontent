2fcc3dd3ba85851218b74fe7c783bbd17799fb55
Naresh Golla <mrgollanaresh95@gmail.com>
2026-03-05 20:21:46 +0530

    fixed api urls

--- src/Comp/Register.jsx
+++ src/Comp/Register.jsx
@@ -29,7 +29,7 @@ const Register = () => {
       setLoading(true);
 
       const response = await fetch(
-        "http://localhost:5000/api/auth/register",
+        "https://citisolve-backend-new.onrender.com/api/auth/register",
         {
           method: "POST",
           headers: {
